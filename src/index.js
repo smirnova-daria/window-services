8d0fe34d6619a11045e7cb9272e1fe691206bc1a
Daria Smirnova <darya.smirn1@yandex.ru>
2022-05-12 16:28:14 +0300

    Step2 - открытие модального окна по клику на кнопки Заказать звонок, Вызвать замерщика

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ import {
 }
 from './modules/modal'
 
-modal()
+modal({
+	elementSelector: '.callback',
+	modalSelector: '.header-modal',
+	overlaySelector: '.overlay',
+	closeSelector: '.header-modal__close',
+})
+
+modal({
+	elementSelector: '.measurer-call',
+	modalSelector: '.services-modal',
+	overlaySelector: '.overlay',
+	closeSelector: '.services-modal__close',
+})
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,8 +1,59 @@
+import {
+	animate
+} from './helpers'
+
 export const modal = ({
 	elementSelector,
 	modalSelector,
 	overlaySelector,
-	closeSelector
+	closeSelector,
+	animationDuration = 300
 }) => {
-console.log('two')
+	try {
+		const buttons = document.querySelectorAll(elementSelector)
+		const modal = document.querySelector(modalSelector)
+		const overlay = document.querySelector(overlaySelector)
+		const closeBtn = modal.querySelector(closeSelector)
+
+		const openModal = (e) => {
+			e.preventDefault()
+			modal.style.display = 'block'
+			overlay.style.display = 'block'
+			animate({
+				duration: animationDuration,
+				timing(timeFraction) {
+					return timeFraction;
+				},
+				draw(progress) {
+					modal.style.opacity = progress
+				}
+			})
+		}
+
+		const closeModal = () => {
+			animate({
+				duration: animationDuration,
+				timing(timeFraction) {
+					return timeFraction;
+				},
+				draw(progress) {
+					modal.style.opacity = 1 - progress
+				}
+			})
+			setTimeout(() => {
+				modal.style.display = 'none'
+				overlay.style.display = 'none'
+			}, animationDuration)
+
+		}
+
+		buttons.forEach(button => {
+			button.addEventListener('click', openModal)
+		})
+
+		closeBtn.addEventListener('click', closeModal)
+
+	} catch (error) {
+		console.log(error.message)
+	}
 }
