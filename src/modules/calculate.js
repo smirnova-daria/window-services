ec8038835873951620234ecb51f11332a71d4e77
Daria Smirnova <darya.smirn1@yandex.ru>
2022-05-13 20:46:32 +0300

    Step7 - реализован калькулятор на странице Балконы

--- src/modules/calculate.js
+++ src/modules/calculate.js
@@ -4,45 +4,46 @@ import {
 
 export const calculate = () => {
 	const calcBlock = document.getElementById('calc')
-	const calcType = document.getElementById('calc-type')
-	const calcTypeMaterial = document.getElementById('calc-type-material')
-	const calcSquare = document.getElementById('calc-input')
-	const total = document.getElementById('calc-total')
+	if (calcBlock) {
+		const calcType = document.getElementById('calc-type')
+		const calcTypeMaterial = document.getElementById('calc-type-material')
+		const calcSquare = document.getElementById('calc-input')
+		const total = document.getElementById('calc-total')
 
-	const countCalc = () => {
-		const calcTypeValue = +calcType.options[calcType.selectedIndex].value
-		const calcSquareValue = +calcSquare.value
-		const calcTypeMaterialValue = +calcTypeMaterial.options[calcTypeMaterial.selectedIndex].value
+		const countCalc = () => {
+			const calcTypeValue = +calcType.options[calcType.selectedIndex].value
+			const calcSquareValue = +calcSquare.value
+			const calcTypeMaterialValue = +calcTypeMaterial.options[calcTypeMaterial.selectedIndex].value
 
-		let totalValue = 0
+			let totalValue = 0
 
-		if (calcTypeValue && calcSquareValue && calcTypeMaterialValue) {
-			totalValue = calcSquareValue * calcTypeValue * calcTypeMaterialValue
-		} else if (calcTypeValue && calcSquareValue) {
-			totalValue = calcSquareValue * calcTypeValue
-		} else {
-			totalValue = 0
+			if (calcTypeValue && calcSquareValue && calcTypeMaterialValue) {
+				totalValue = calcSquareValue * calcTypeValue * calcTypeMaterialValue
+			} else if (calcTypeValue && calcSquareValue) {
+				totalValue = calcSquareValue * calcTypeValue
+			} else {
+				totalValue = 0
+			}
+			return totalValue
 		}
-		return totalValue
+
+		calcBlock.addEventListener('input', e => {
+			if (e.target === calcSquare) {
+				e.target.value = e.target.value.replace(/\D+/g, '')
+			}
+			if (e.target === calcType || e.target === calcTypeMaterial || e.target === calcSquare) {
+				const totalValue = countCalc()
+
+				animate({
+					duration: 300,
+					timing(timeFraction) {
+						return timeFraction;
+					},
+					draw(progress) {
+						total.value = Math.round(progress * totalValue)
+					}
+				});
+			}
+		})
 	}
-
-	calcBlock.addEventListener('input', e => {
-		if (e.target === calcSquare) {
-			e.target.value = e.target.value.replace(/\D+/g, '')
-		}
-		if (e.target === calcType || e.target === calcTypeMaterial || e.target === calcSquare) {
-			const totalValue = countCalc()
-
-			animate({
-				duration: 300,
-				timing(timeFraction) {
-					return timeFraction;
-				},
-				draw(progress) {
-					total.value = Math.round(progress * totalValue)
-				}
-			});
-		}
-	})
-
 }
