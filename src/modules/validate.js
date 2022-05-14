7e7feea3a3904428ed08061e7acac78a30161249
Daria Smirnova <darya.smirn1@yandex.ru>
2022-05-14 16:03:31 +0300

    Внесены правки: удаление пробелов из поля Имя, выделение цветом невалидных полей, корректировка валидации при отправке формы

--- src/modules/validate.js
+++ src/modules/validate.js
@@ -1,8 +1,10 @@
 export const validateInput = (input) => {
 	if (input.value) {
 		if (input.name === 'fio') {
+			input.classList.remove('error')
 			input.value = input.value.replace(/[^а-яА-Яa-zA-Z-\s]/g, '')
 		} else if (input.name === 'phone') {
+			input.classList.remove('error')
 			input.value = input.value.replace(/[^\+\d]/g, '')
 		}
 	}
@@ -11,34 +13,30 @@ export const validateInput = (input) => {
 export const isValidatedForm = (form) => {
 	const inputsList = form.querySelectorAll('input')
 
-	let success = true
 	let fioSuccess = false
 	let phoneSuccess = false
 
 	if (inputsList) {
 		inputsList.forEach(input => {
-			if (input.value === '') {
-				success = false
-			} else {
+			if (input.name === 'fio') {
+				input.value = input.value.trim()
+				fioSuccess = !!input.value && /[а-яА-Яa-zA-Z-\s]/g.test(input.value)
 
-				if (input.name === 'fio') {
-					if (/[а-яА-Яa-zA-Z-\s]/g.test(input.value)) {
-						fioSuccess = true
-					} else {
-						fioSuccess = false
-					}
-				}
-
-				if ((input.name === 'phone')) {
-					if (/^([+]?[0-9]{11,16})*$/g.test(input.value)) {
-						phoneSuccess = true
-					} else {
-						phoneSuccess = false
-					}
+				if (!fioSuccess) {
+					input.classList.add('error')
 				}
 			}
+
+			if ((input.name === 'phone')) {
+				phoneSuccess = !!input.value && /^([+]?[0-9]{5,16})/g.test(input.value)
+
+				if (!phoneSuccess) {
+					input.classList.add('error')
+				}
+			}
+
 		})
 	}
 
-	return success && fioSuccess && phoneSuccess
+	return fioSuccess && phoneSuccess
 }
