332d0d22e42141b267241ea7648b688d5b650ccb
Daria Smirnova <darya.smirn1@yandex.ru>
2022-06-07 19:40:19 +0300

    Обновление таймера; Перемещение файла для публикации на Github Pages; Обновление путей к файлам

--- src/index.js
+++ src/index.js
@@ -1,71 +1,60 @@
-import {
-	modal
-}
-from './modules/modal'
-import {
-	timer
-} from './modules/timer'
-import {
-	smoothScroll
-} from './modules/smoothScroll'
-import {
-	sliderServices
-} from './modules/swiper'
-import {
-	calculate
-} from './modules/calculate'
-import {
-	sendForm
-} from './modules/sendForm'
-import {
-	comments
-} from './modules/comments'
+import { modal } from './modules/modal'
+import { timer } from './modules/timer'
+import { smoothScroll } from './modules/smoothScroll'
+import { sliderServices } from './modules/swiper'
+import { calculate } from './modules/calculate'
+import { sendForm } from './modules/sendForm'
+import { comments } from './modules/comments'
 
 modal({
-	elementSelector: '.callback',
-	modalSelector: '.header-modal',
-	overlaySelector: '.overlay',
-	closeSelector: '.header-modal__close',
+    elementSelector: '.callback',
+    modalSelector: '.header-modal',
+    overlaySelector: '.overlay',
+    closeSelector: '.header-modal__close',
 })
 
 modal({
-	elementSelector: 'measurer-call',
-	modalSelector: '.services-modal',
-	overlaySelector: '.overlay',
-	closeSelector: '.services-modal__close',
-	isServices: true,
-	servicesBlockSelector: '#services',
+    elementSelector: 'measurer-call',
+    modalSelector: '.services-modal',
+    overlaySelector: '.overlay',
+    closeSelector: '.services-modal__close',
+    isServices: true,
+    servicesBlockSelector: '#services',
 })
 
 modal({
-	elementSelector: '.document-overlay',
-	modalSelector: '.image-modal',
-	overlaySelector: '.overlay',
-	closeSelector: '.image-modal__close',
-	isImageModal: true,
-	boxElementSelector: '.sertificate-document',
-	modalBodySelector: '.image-modal__body'
+    elementSelector: '.document-overlay',
+    modalSelector: '.image-modal',
+    overlaySelector: '.overlay',
+    closeSelector: '.image-modal__close',
+    isImageModal: true,
+    boxElementSelector: '.sertificate-document',
+    modalBodySelector: '.image-modal__body',
 })
 
-timer('20 may 2022 21:00')
+timer('20 june 2022 21:00')
 
 smoothScroll()
 
 calculate()
 
 sendForm({
-	formSelector: '[name="action-form"]',
-	additionalData: [{
-		type: 'input',
-		selector: '#calc-total'
-	}]
+    formSelector: '[name="action-form"]',
+    additionalData: [
+        {
+            type: 'input',
+            selector: '#calc-total',
+        },
+    ],
 })
 sendForm({
-	formSelector: '[name="action-form2"]',
-	additionalData: [{
-		type: 'input',
-		selector: '#calc-total'
-	}]
+    formSelector: '[name="action-form2"]',
+    additionalData: [
+        {
+            type: 'input',
+            selector: '#calc-total',
+        },
+    ],
 })
 
 comments()
--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,117 +1,123 @@
 export const comments = () => {
-	const reviewsBlock = document.querySelector('#reviews .comments-container')
+    const reviewsBlock = document.querySelector('#reviews .comments-container')
 
-	//время для смены блоков с комментариями
-	const timerInterval = 20000
+    //время для смены блоков с комментариями
+    const timerInterval = 20000
 
-	let hideSlide = 0
-	let openSlide = 3
-	let openSlidesCount = 3
+    let hideSlide = 0
+    let openSlide = 3
+    let openSlidesCount = 3
 
-	let interval
+    let interval
 
-	const getData = (url) => {
-		return fetch(url)
-			.then(res => res.json())
-			.catch(error => console.log(error.message))
-	}
+    const getData = (url) => {
+        return fetch(url)
+            .then((res) => res.json())
+            .catch((error) => console.log(error.message))
+    }
 
+    const render = (comments = []) => {
+        const reviewColors = ['review-green', 'review-gray', 'review-orange']
 
-	const render = (comments = []) => {
-		const reviewColors = ['review-green', 'review-gray', 'review-orange']
+        const commentItems = []
 
-		const commentItems = []
+        comments.forEach((comment, index) => {
+            const commentItem = document.createElement('div')
+            commentItem.className = `row review-margin-bottom comment-item`
 
-		comments.forEach((comment, index) => {
-			const commentItem = document.createElement('div')
-			commentItem.className = `row review-margin-bottom comment-item`
-
-			if (comment.id % 2 === 0) {
-				commentItem.innerHTML = `					
+            if (comment.id % 2 === 0) {
+                commentItem.innerHTML = `					
 						<div class="col-xs-9 col-sm-9">
-							<div class="review-inner ${reviewColors[comment.id%reviewColors.length]} review-arrow review-arrow-right">
+							<div class="review-inner ${
+                                reviewColors[comment.id % reviewColors.length]
+                            } review-arrow review-arrow-right">
 								<p class="text-normal">${comment.author}</p>
 								<p>${comment.comment}</p>
 							</div>
 						</div>
 						<div class="col-xs-3 col-sm-2">
 							<div class="review-user">
-								<img src="images/users/${comment.image || 'userpic.png'}" alt="user avatar" class="img-responsive avatar">
+								<img src="/dist/images/users/${
+                                    comment.image || 'userpic.png'
+                                }" alt="user avatar" class="img-responsive avatar">
 							</div>
 						</div>					
 				`
-			} else {
-				commentItem.innerHTML = `				
+            } else {
+                commentItem.innerHTML = `				
 						<div class="col-xs-3 col-sm-2">
 							<div class="review-user">
-								<img src="images/users/${comment.image || 'userpic.png'}" alt="user avatar" class="img-responsive avatar">
+								<img src="/dist/images/users/${
+                                    comment.image || 'userpic.png'
+                                }" alt="user avatar" class="img-responsive avatar">
 							</div>
 						</div>
 						<div class="col-xs-9 col-sm-9">
-							<div class="review-inner ${reviewColors[comment.id%reviewColors.length]} review-arrow review-arrow-left">
+							<div class="review-inner ${
+                                reviewColors[comment.id % reviewColors.length]
+                            } review-arrow review-arrow-left">
 								<p class="text-normal">${comment.author}</p>
 								<p>${comment.comment}</p>
 							</div>
 						</div>					
 				`
-			}
+            }
 
-			//Блок для правильного отображения стрелки к цитате отзыва
-			if (index < openSlidesCount) {
-				reviewsBlock.append(commentItem)
-				const arrowBlock = commentItem.querySelector('.review-arrow')
-				arrowBlock.classList.add('rotate')
-			}
+            //Блок для правильного отображения стрелки к цитате отзыва
+            if (index < openSlidesCount) {
+                reviewsBlock.append(commentItem)
+                const arrowBlock = commentItem.querySelector('.review-arrow')
+                arrowBlock.classList.add('rotate')
+            }
 
-			commentItems.push(commentItem)
+            commentItems.push(commentItem)
+        })
 
-		})
+        startSlide(commentItems, timerInterval)
+    }
 
-		startSlide(commentItems, timerInterval)
+    const toHideSlide = (elems, index) => {
+        elems[index].remove()
+    }
 
-	}
+    const toOpenSlide = (elems, index) => {
+        reviewsBlock.append(elems[index])
+    }
 
-	const toHideSlide = (elems, index) => {
-		elems[index].remove()
-	}
+    const autoSlide = (slides) => {
+        toHideSlide(slides, hideSlide)
+        hideSlide++
+        if (hideSlide >= slides.length) {
+            hideSlide = 0
+        }
 
-	const toOpenSlide = (elems, index) => {
-		reviewsBlock.append(elems[index])
-	}
+        toOpenSlide(slides, openSlide)
+        openSlide++
+        if (openSlide >= slides.length) {
+            openSlide = 0
+        }
+    }
 
+    const startSlide = (slides, timer = 5000) => {
+        interval = setInterval(() => {
+            autoSlide(slides)
+        }, timer)
+    }
 
-	const autoSlide = (slides) => {
-		toHideSlide(slides, hideSlide)
-		hideSlide++
-		if (hideSlide >= slides.length) {
-			hideSlide = 0
-		}
+    reviewsBlock.innerHTML = `<div style="padding-left:33%;"><img src="/dist/images/spinner.svg" alt="spinner"></div>`
 
-		toOpenSlide(slides, openSlide)
-		openSlide++
-		if (openSlide >= slides.length) {
-			openSlide = 0
-		}
-	}
-
-	const startSlide = (slides, timer = 5000) => {
-		interval = setInterval(() => {
-			autoSlide(slides)
-		}, timer)
-	}
-
-	reviewsBlock.innerHTML = `<div style="padding-left:33%;"><img src="images/spinner.svg" alt="spinner"></div>`
-
-	getData('comments.json').then(data => {
-		//Добавила SetTimeout, чтобы нагляднее отобразить момент загрузки комментариев с "сервера"
-		setTimeout(() => {
-			try {
-				reviewsBlock.innerHTML = ''
-				render(data.comments)
-			} catch (error) {
-				console.log(error.message)
-				reviewsBlock.innerHTML = `<div style="padding-left:33%;"><img src="images/spinner.svg" alt="spinner"></div>`
-			}
-		}, 2000)
-	}).catch(error => console.log(error.message))
+    getData('/dist/comments.json')
+        .then((data) => {
+            //Добавила SetTimeout, чтобы нагляднее отобразить момент загрузки комментариев с "сервера"
+            setTimeout(() => {
+                try {
+                    reviewsBlock.innerHTML = ''
+                    render(data.comments)
+                } catch (error) {
+                    console.log(error.message)
+                    reviewsBlock.innerHTML = `<div style="padding-left:33%;"><img src="/dist/images/spinner.svg" alt="spinner"></div>`
+                }
+            }, 2000)
+        })
+        .catch((error) => console.log(error.message))
 }
